89856f52d40ca27f47d478bf79ddf58defdf47b3
Gérald Barbosa <barbosa.gerald@gmail.com>
2019-09-18 22:47:12 +0200

    index.html renommé en index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>        
+        <meta charset="UTF-8">
+        <meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=1.0' />
+        <title>Your title</title>
+        <link rel="stylesheet" href="your_style.css">
+        <script src="your_script.js"></script>
+    </head>
+    <body>
+<?php
+echo 'Le PHP est bien interprété...';
+?>        <!-- CONTENT -->
+    </body>
+</html>
